7b0906ac2b9c23ee519f855d3696b894ca91cc18
makombengas <berlioz2001@yahoo.fr>
2024-05-20 08:56:34 +0200

    leatleaf icon modified

--- src/components/blueMap/BlueMap.jsx
+++ src/components/blueMap/BlueMap.jsx
@@ -16,11 +16,10 @@ L.Icon.Default.mergeOptions({
 });
 const customIcon = new L.Icon({
   iconUrl: '/place.svg',
-  iconSize: [25, 41], // size of the icon
+  iconSize: [35, 41], // size of the icon
   iconAnchor: [12, 41], // point of the icon which will correspond to marker's location
   popupAnchor: [1, -34], // point from which the popup should open relative to the iconAnchor
-  shadowUrl: '/place.svg',
-  shadowSize: [41, 41],
+ 
 });
 
 const BlueMap = () => {
